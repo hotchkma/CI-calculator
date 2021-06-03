dd3f5ad4865948bc0080b16474d54f887ef81005
hotchkma <63124213+hotchkma@users.noreply.github.com>
2021-06-03 13:53:50 -0700

    add multiply test

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,6 @@ class TestCalculatorApp:
     
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
+        
+    def test_multiply(self):
+        assert 6 == calculator.multiply(3, 2)
