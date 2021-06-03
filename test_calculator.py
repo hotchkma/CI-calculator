bb6ad7c44da8ef0fe3227e353c224f27db6505f8
hotchkma <63124213+hotchkma@users.noreply.github.com>
2021-06-03 13:56:24 -0700

    Update test_calculator.py
    added divide test

--- test_calculator.py
+++ test_calculator.py
@@ -14,3 +14,6 @@ class TestCalculatorApp:
         
     def test_multiply(self):
         assert 6 == calculator.multiply(3, 2)
+
+    def test_divide(self):
+        assert 5 == calculator.divide(11, 2)
